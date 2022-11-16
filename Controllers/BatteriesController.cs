ea81d2f4a7dcb29e137ba077f2a7b812dddb0e48
Carojas14 <carorojasdevia@gmail.com>
2022-11-16 10:38:42 -0500

    first commit

---
+++ Controllers/BatteriesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Rocket_Elevators_Rest_API.Models;
+
+
+namespace Rocket_Elevators_Rest_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BatteriesController : ControllerBase
+    {
+        private readonly RocketElevatorsContext _context;
+
+        public BatteriesController(RocketElevatorsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<Batteries>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Battery>>> GetBatteries()
+        {
+            return await _context.batteries.ToListAsync();
+        }
+
+        // GET api/Batteries/id
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<BatteriesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<BatteriesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BatteriesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/Battery.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Diagnostics.CodeAnalysis;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Rocket_Elevators_Rest_API.Models
+{
+    public class Battery
+    {
+
+        
+        [Key]
+        public int id { get; set; }
+        public int building_id { get; set; }
+        public string battery_type { get; set; } = null!;
+        public string status { get; set; } = null!;
+
+
+
+    }
+}
